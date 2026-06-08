e2b59e254760df827d662ca0d124c90da4c9eada
joune-on-macbookOS-monterey <119015940+jouneisamonth@users.noreply.github.com>
2026-06-08 15:44:22 -0400

    clean: README only, untrack branches/

--- branches/main/src/services/storageService.js
+++
@@ -1,8 +0,0 @@
-import { storageService as fileService }  from './fileStorageService'
-import { storageService as localService } from './localStorageService'
-
-// Electron shell always serves from localhost; GitHub Pages never does.
-const isLocal = window.location.hostname === 'localhost' ||
-                window.location.hostname === '127.0.0.1'
-
-export const storageService = isLocal ? fileService : localService
